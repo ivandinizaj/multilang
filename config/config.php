99bb7a3bf809260b357893c50a99668ca0b62283
Ivan Diniz <ivandinizaj@gmail.com>
2014-12-15 18:29:56 -0300

    Add All

--- config/config.php
+++ config/config.php
@@ -3,9 +3,9 @@ define('DIR',realpath(__DIR__.'/..'));
 define('PATH_LANG',DIR.'/lang');
 
 
-
 $protocolo = (strpos(strtolower($_SERVER['SERVER_PROTOCOL']), 'https') === false) ? 'http' : 'https'; 
 $host = $_SERVER['HTTP_HOST'];
+
 
 define('HOSTNAME',$host);
 define('BASE_URL',$protocolo.'://'.$host);
